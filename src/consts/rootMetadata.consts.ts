1fab179ae2c286e50d7bcfbb27bd9382f952176f
Roman Rozanov <antifreez@gmail.com>
2025-01-20 15:09:47 +0200

    basic implememnbtaion of metadata

---
+++ src/consts/rootMetadata.consts.ts
@@ -0,0 +1,47 @@
+import { Metadata } from 'next';
+
+export const ROOT_METADATA: Metadata = {
+    title: 'Antifreez CSS Art',
+    description: `Antifreez CSS Art is a creative web project by Roman Rozanov, showcasing visually stunning artworks crafted purely with HTML and CSS. From intricate illustrations to dynamic visual effects, the project pushes the boundaries of what’s possible with front-end technologies—proving that CSS is not just for styling but also a powerful tool for artistic expression.
+Whether you're a designer looking for inspiration or a developer eager to explore the artistic side of coding, Antifreez CSS Art offers a unique blend of creativity and technical precision. Stay tuned for regular updates, behind-the-scenes breakdowns, and tips on mastering CSS for art and animation.`,
+    keywords: [
+        'CSS art',
+        'HTML CSS design',
+        'CSS illustrations',
+        'front-end art',
+        'web design',
+        'Roman Rozanov',
+        'Antifreez CSS Art',
+        'creative coding',
+        'CSS animations',
+        'pure CSS art',
+        'web development',
+        'front-end development',
+        'digital art',
+        'web graphics',
+        'CSS tricks',
+        'modern web design',
+        'UI/UX design',
+        'CSS effects',
+        'responsive design',
+        'web animations',
+        'CSS creativity',
+        'coding for designers',
+        'artistic web projects',
+        'visual web design',
+    ],
+    openGraph: {
+        title: 'Antifreez CSS Art',
+        description: 'Stunning art crafted with pure CSS.',
+        url: 'https://antifreez-art.com',
+        siteName: 'Antifreez CSS Art',
+        images: [
+            {
+                url: '/icons/icon-512x512.png',
+                width: 512,
+                height: 512,
+            },
+        ],
+        type: 'website',
+    },
+};
