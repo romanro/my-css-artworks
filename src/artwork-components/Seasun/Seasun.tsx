db4b432a03bd74b95e7107e14aebaf8d2933bbf3
Roman Rozanov <romanrozanov@ROMANs-MacBook-Pro.local>
2025-03-13 09:46:43 +0200

    Enhance Seasun component with sun element and improved styling; update MainNavbar z-index

--- src/artwork-components/Seasun/Seasun.tsx
+++ src/artwork-components/Seasun/Seasun.tsx
@@ -5,6 +5,7 @@ export const Seasun: FC = () => {
     return (
         <div className={`${styles.container}`}>
             <section className={`${styles.sky}`}></section>
+            <div className={`${styles.sun}`}></div>
             <section className={`${styles.sea}`}></section>
         </div>
     );
